164d418ef7a84b3e0fe8a596d90cae8acb8e216f
Sheena Vasani <sheenavasani1@gmail.com>
2017-07-13 16:46:00 -0700

    fixed delete all and check all bug

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
+//for each item user submit, add to list
+
 $(function() {
   
-  
-
-
-
 //Event listeners
 
 
@@ -13,17 +11,18 @@ $('.js-form').submit(function(event) {
  //for each input user submits, store and keep placing in list
 //add input to list
   
-  $(".js-display-user-text").append('<li> Item: ' + userTextElement.val() + '</li>' + 
-    '<button class="delete">Delete</button><button class="check">Check</button>');
+  $(".js-display-user-text").append('<li> Item: ' + userTextElement.val()  + 
+    '<button class="delete">Delete</button><button class="check">Check</button> </li>');
   userTextElement.val("");
     $('.delete').click(function () { 
-          $(this).parent().remove();
+          $(this).parent("li").remove();
+     // console.log($(this).parent("li"));
 
 
   });
   
    $(".check").click(function (event) {
-   $(this).parent().css("text-decoration", "line-through");
+   $(this).parent("li").css("text-decoration", "line-through");
 
 });
   
